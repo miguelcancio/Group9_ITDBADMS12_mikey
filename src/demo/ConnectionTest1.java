6cd80f669370b60e4609fc68fd7d0bd3e9bb7fcd
angelica <uniunicornbae@gmail.com>
2025-07-13 01:37:24 +0800

    Initial commit

---
+++ src/demo/ConnectionTest1.java
@@ -0,0 +1,21 @@
+package demo;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionTest1 {
+	public static void main(String[] args) {
+		String url = "jdbc:mysql://localhost:3306/sakila";
+		String user = "root";
+		String password = "Dlsu1234!";
+		
+		try {
+			Connection conn = DriverManager.getConnection(url, user, password);
+			System.out.println("Successfully connected MySQL");
+			conn.close();
+		} catch (SQLException e) {
+			System.out.println("Connection failed.");
+			e.printStackTrace();
+		}
+	}
+}
